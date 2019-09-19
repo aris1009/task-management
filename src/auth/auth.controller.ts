5da35563f8dedb4e65bbd1b3f0e58c92cda5c2bd
Aris Konstantoulas <aris.konstantoulas@ist.com.gr>
2019-09-19 16:50:30 +0300

    JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UseGuards, Req } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -19,7 +20,13 @@ export class AuthController {
     @Post('/signin')
     signin(
         @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto
-    ): Promise<string> {
+    ): Promise<{ accessToken: string }> {
         return this.authService.signin(authCredentialsDto);
+    }
+
+    @Post('/test')
+    @UseGuards(AuthGuard())
+    test(@Req() req) {
+        console.log(req.user);
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { UserRepository } from './user.repository';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserRepository])
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'someSecret',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    JwtStrategy,
+    AuthService
+  ],
+  exports: [
+    JwtStrategy,
+    AuthService
+  ]
 })
 export class AuthModule { }
